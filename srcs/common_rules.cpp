0165cb1238afee57d0a6037b538f9a1b0690ea5f
Касим Байбиков <kasim.baybikov@gmail.com>
2021-09-28 02:08:00 +0300

    custom rules

--- srcs/common_rules.cpp
+++ srcs/common_rules.cpp
@@ -3,16 +3,28 @@
 
 void Generation::common_rules(std::vector<std::string> &copy_vector, int neighbors, int i, int j)
 {
+	//if (this->map[i][j] == ' ' && neighbors == 3)
+	//{
+		//copy_vector[i][j] = this->symbol;
+		//this->survivors++;
+	//}
+	//else if (this->map[i][j] != ' ' && (neighbors == 3 || neighbors == 2))
+	//{
+		//copy_vector[i][j] = this->symbol;
+		//this->survivors++;
+	//}
+	//else
+		//copy_vector[i][j] = ' ';
 	if (this->map[i][j] == ' ' && neighbors == 3)
 	{
 		copy_vector[i][j] = this->symbol;
 		this->survivors++;
 	}
-	else if (this->map[i][j] != ' ' && (neighbors == 3 || neighbors == 2))
+	else if (this->map[i][j] != ' ' && (neighbors >= 1))
 	{
 		copy_vector[i][j] = this->symbol;
 		this->survivors++;
 	}
-	else if (this->map[i][j] != ' ' && (neighbors > 3 || neighbors < 2))
+	else
 		copy_vector[i][j] = ' ';
 }
